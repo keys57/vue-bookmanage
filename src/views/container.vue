58fd1752aeeeab2745f66049ccd6b283d74f8966
keys57 <hx0057@gmail.com>
2023-01-15 21:29:16 +0800

    add layout

--- src/views/container.vue
+++ src/views/container.vue
@@ -42,7 +42,14 @@
       </el-menu>
           </el-aside>
           <el-container>
-            <el-main>Main</el-main>
+            <el-main>
+                <el-row :gutter="20">
+                    <el-col :span="6"><div class="grid-content ep-bg-purple" >1</div></el-col>
+                    <el-col :span="6"><div class="grid-content ep-bg-purple" >2</div></el-col>
+                    <el-col :span="6"><div class="grid-content ep-bg-purple" >3</div></el-col>
+                    <el-col :span="6"><div class="grid-content ep-bg-purple" >4</div></el-col>
+                </el-row>
+            </el-main>
             <el-footer>Footer</el-footer>
           </el-container>
         </el-container>
@@ -64,11 +71,17 @@
     }
     .el-main {
         text-align: center;
-        background-color: bisque;
+        /* background-color: bisque; */
     }
     .el-footer {
         text-align: center;
         background-color:blue
+    }
+    .ep-bg-purple{
+        background-color: purple;
+        color:white;
+        text-align: center;
+        height: 100px;
     }
 </style>
   
